17ff684b98ed843836d7a0fc1df3f291148722a7
msn9110 <msn9110@gmail.com>
2016-09-10 00:30:49 +0800

    Add files via upload

--- app/src/main/java/com/example/mytalker/StartMenu.java
+++ app/src/main/java/com/example/mytalker/StartMenu.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class StartMenu extends Activity{
-    Button[] mode=new Button[6];
+    Button[] mode=new Button[4];
     LayoutInflater inflater;
 
     @Override
@@ -22,11 +22,31 @@ public class StartMenu extends Activity{
         mode[1]=(Button)findViewById(R.id.btn_mode2);
         mode[2]=(Button)findViewById(R.id.btn_mode3);
         mode[3]=(Button)findViewById(R.id.btn_mode4);
-        mode[4]=(Button)findViewById(R.id.btn_mode5);
-        mode[5]=(Button)findViewById(R.id.btn_mode6);
-
 
         mode[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                InputActivity.con=false;
+                Intent intent =new Intent();
+                intent.setClass(getApplicationContext(),InputActivity.class);
+                startActivity(intent);
+                //StartMenu.this.finish();
+            }
+        });
+
+        mode[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SpeechMode.con=false;
+                SpeechMode.path="Main";
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), SpeechMode.class);
+                startActivity(intent);
+            }
+        });
+
+        mode[2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 InputActivity.con=true;
@@ -38,50 +58,11 @@ public class StartMenu extends Activity{
             }
         });
 
-        mode[1].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                InputActivity.con=false;
-                Intent intent =new Intent();
-                intent.setClass(getApplicationContext(),InputActivity.class);
-                startActivity(intent);
-                //StartMenu.this.finish();
-            }
-        });
-
-        mode[2].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent =new Intent();
-                intent.setClass(getApplicationContext(), DBActivity.class);
-                startActivity(intent);
-            }
-        });
-
         mode[3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(getApplicationContext(), DataMove.class);
-                startActivity(intent);
-            }
-        });
-
-        mode[4].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SpeechMode.con=false;
-                SpeechMode.path="Main";
-                Intent intent = new Intent();
-                intent.setClass(getApplicationContext(), SpeechMode.class);
-                startActivity(intent);
-            }
-        });
-        mode[5].setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(getApplicationContext(), Info.class);
                 startActivity(intent);
             }
         });
