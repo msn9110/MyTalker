25bb7833616b65f634f364cb78a44ccdc60c6c71
msn9110 <msn9110@gmail.com>
2016-10-09 19:46:07 +0800

    wifi hospot

--- app/src/main/java/com/example/mytalker/DisplayActivity.java
+++ app/src/main/java/com/example/mytalker/DisplayActivity.java
@@ -19,10 +19,11 @@ public class DisplayActivity extends Activity {
     public static final String END="!!!@@@###";
     public static final int PORT = 8988;
     public boolean terminal=true;
+    public static boolean WifiMode=true;
     String[] buf=new String[100];//queue for playing tts
     int count=0;
     TextView tvDisplay,tvStatus;
-    private  Handler handler;
+    private  Handler handler=new Handler();
     private ServerSocket serverSocket=null;
     Speaker speaker;
 
@@ -38,41 +39,25 @@ public class DisplayActivity extends Activity {
         tvStatus=(TextView)findViewById(R.id.textView3);
         tvDisplay.setText(R.string.empty);
     }
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        speaker.shutdown();
-        this.finish();
-    }
 
-    @Override
-    protected void onPause(){
-        super.onPause();
-        try {
-            serverSocket.close();
-        }catch (IOException e){
-            Log.d(WiFiDirectActivity.TAG,e.toString());
-        }
-    }
-    int current=0;
+
     @Override
     protected void onStart(){
         super.onStart();
-        handler=new Handler();
+        if(WifiMode)
+            new Thread(socket_server).start();
 
-        //建立Thread
-        Thread server = new Thread(socket_server);
-        //啟動Thread
-        server.start();
     }
-    boolean end=false;
+
     ReentrantLock lock=new ReentrantLock();
+    int current=0;
+    boolean EndDisplay=false;
     String message="";
     Thread display=new Thread(new Runnable() {
         @Override
         public void run() {
 
-            while (!end){
+            while (!EndDisplay){
 
                 if(count>0 && speaker.isNotSpeaking()){
                     message=buf[current];
@@ -80,23 +65,14 @@ public class DisplayActivity extends Activity {
                     System.out.println("COUNT : "+count);
                     handler.post(new Runnable() {
                         public void run() {
-                            int size=message.length();
-                            float font=120;
-                            if(size>30)
-                                font=100;
-                            else if(size>80)
-                                font=75;
-                            else if(size>120)
-                                font=50;
                             if(message.length()>0 && speaker.isNotSpeaking()) {
                                 lock.lock();
-                                tvDisplay.setTextSize(font);
+                                int font=6000/(message.length()+40);
+                                tvDisplay.setTextSize((float)font);
                                 tvDisplay.setText(message);
                                 speaker.speak(message);
-                                message="";
-                                buf[current]="";
-                                current++;
-                                current%=100;
+                                message=buf[current]="";
+                                current=(current+1) % buf.length;
                                 count--;
                                 lock.unlock();
                             }
@@ -140,8 +116,7 @@ public class DisplayActivity extends Activity {
                             System.out.println("COUNT : "+count);
                             buf[i]=line;
                             System.out.println("Receive "+i+" : "+buf[i]);
-                            i++;
-                            i%=100;
+                            i=(i+1) % buf.length;
                             lock.lock();
                             count++;
                             lock.unlock();
@@ -155,7 +130,7 @@ public class DisplayActivity extends Activity {
                         }
                     });
                     in.close();
-                    end=true;
+                    EndDisplay=true;
                     terminal=false;
                     DisplayActivity.this.finish();
                 }
@@ -166,10 +141,28 @@ public class DisplayActivity extends Activity {
                         tvStatus.setText(err);
                     }
                 });
-                end=true;
+                EndDisplay=true;
                 terminal=false;
                 DisplayActivity.this.finish();
             }
         }
     };
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        speaker.shutdown();
+        this.finish();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        try {
+            if (WifiMode)
+                serverSocket.close();
+        }catch (IOException e){
+            Log.d(WiFiDirectActivity.TAG,e.toString());
+        }
+    }
 }
