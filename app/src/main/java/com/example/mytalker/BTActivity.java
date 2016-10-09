5e19e45d4ac88e23537477eb4cff1dd865df80d4
msn9110 <msn9110@gmail.com>
2016-10-09 21:28:28 +0800

    BT

--- app/src/main/java/com/example/mytalker/BTActivity.java
+++ app/src/main/java/com/example/mytalker/BTActivity.java
@@ -2,12 +2,34 @@ package com.example.mytalker;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BTActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+        Button client=(Button)findViewById(R.id.button);
+        Button server=(Button)findViewById(R.id.button2);
+        client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setClass(BTActivity.this,InputActivity.class);
+                startActivity(intent);
+            }
+        });
+        server.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setClass(BTActivity.this,DisplayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
