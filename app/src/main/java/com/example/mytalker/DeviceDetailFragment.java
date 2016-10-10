8f7ab4eb5438a667fe12b3b5efbe96cb9653d76d
msn9110 <msn9110@gmail.com>
2016-10-10 18:43:15 +0800

    wifi ap

--- app/src/main/java/com/example/mytalker/DeviceDetailFragment.java
+++ app/src/main/java/com/example/mytalker/DeviceDetailFragment.java
@@ -168,30 +168,4 @@ public class DeviceDetailFragment extends Fragment implements ConnectionInfoList
         mContentView.findViewById(R.id.btn_start_client).setVisibility(View.GONE);
         this.getView().setVisibility(View.GONE);
     }
-
-
-
-
-
-    public static boolean copyFile(InputStream inputStream, OutputStream out) {
-        byte buf[] = new byte[1024];
-        int len;
-        long startTime=System.currentTimeMillis();
-
-        try {
-            while ((len = inputStream.read(buf)) != -1) {
-                out.write(buf, 0, len);
-            }
-            out.close();
-            inputStream.close();
-            long endTime=System.currentTimeMillis()-startTime;
-            Log.v("","Time taken to transfer all bytes is : "+endTime);
-
-        } catch (IOException e) {
-            Log.d(WiFiDirectActivity.TAG, e.toString());
-            return false;
-        }
-        return true;
-    }
-
 }
